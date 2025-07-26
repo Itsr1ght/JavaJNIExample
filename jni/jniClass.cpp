aeb47c8bafb8909d70e6132a0958722936680606
Bilal <bjlegend007@gmail.com>
2025-07-26 13:01:26 +0530

    simpe java to cpp string passing

--- jni/jniClass.cpp
+++ jni/jniClass.cpp
@@ -1,7 +1,14 @@
 #include "JniClass.h"
 #include <jni.h>
 #include <iostream>
+#include <string>
 
-JNIEXPORT void JNICALL Java_JniClass_print_1hello(JNIEnv *, jobject){
+JNIEXPORT void JNICALL Java_JniClass_print_1hello(JNIEnv *env, jobject jclass){
     std::cout << "Hello World" << std::endl; 
+}
+
+
+JNIEXPORT void JNICALL Java_JniClass_print_1name(JNIEnv *env, jobject jclass, jstring jname){
+    std::string name = (*env).GetStringUTFChars(jname, NULL);
+    std::cout << "Hello" << " " << name << std::endl;
 }
--- src/JniExample.java
+++ src/JniExample.java
@@ -3,6 +3,7 @@
 class JniClass {
 
     public native void print_hello();
+    public native void print_name(String name);
 
     static {
 	System.loadLibrary("native_hello");
@@ -15,6 +16,7 @@ class JniExample {
     public static void main(String[] args){
 	JniClass jni_class = new JniClass();
 	jni_class.print_hello();
+	jni_class.print_name("John");
     }
 
 }
